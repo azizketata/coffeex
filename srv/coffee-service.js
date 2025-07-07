e7ba1b89b96c405b184c170c9128386becc5bb42
Mohamed Aziz Ketata <ge34ram@mytum.de>
2025-07-07 15:06:23 +0200

    Add lowBalanceHandler to CoffeeService for handling low balance notifications

--- srv/coffee-service.js
+++ srv/coffee-service.js
@@ -6,11 +6,13 @@ module.exports = class CoffeeService extends cds.ApplicationService {
     const tapHandler = require('./handlers/tap')
     const batchPayHandler = require('./handlers/batchPay')
     const forecastHandler = require('./handlers/forecast')
+    const lowBalanceHandler = require('./handlers/lowBalance')
     
     // Register handlers
     tapHandler(this)
     batchPayHandler(this)
     forecastHandler(this)
+    lowBalanceHandler(this)
     
     // Continue with default initialization
     return super.init()
